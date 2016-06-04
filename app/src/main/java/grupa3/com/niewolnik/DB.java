a2a4c8498ecfba90fbb30a216c1998940d7e414c
jarekjaro <krzeminskijarek@gmail.com>
2016-06-04 14:53:05 +0200

    testing work day

--- app/src/main/java/grupa3/com/niewolnik/DB.java
+++ app/src/main/java/grupa3/com/niewolnik/DB.java
@@ -29,6 +29,14 @@ public class DB extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(String.format("CREATE TABLE SETTINGS (WEEKDAY CHAR(3) PRIMARY KEY, WORK_MINUTES INT(4) )"));
 
+
+        db.execSQL(String.format("CREATE TABLE REGISTER (LP INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "DATE CHAR(10)," +
+                "A_TIME CHAR(5)," +
+                "L_TIME CHAR(5)," +
+                "FREE_DAY INT(1));"));
+
+        db.execSQL(String.format("CREATE TABLE MONTH_PLAN (DATE CHAR(10) PRIMARY KEY, WORK_MINUTES INT(4) )"));
         addSetting("mon", 480);
         addSetting("tue", 480);
         addSetting("wed", 480);
@@ -36,17 +44,6 @@ public class DB extends SQLiteOpenHelper {
         addSetting("fri", 480);
         addSetting("sat", 480);
         addSetting("sun", 480);
-
-        db.execSQL(
-                String.format("CREATE TABLE REGISTER (LP INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        "DATE CHAR(10)," +
-                        "A_TIME CHAR(5)," +
-                        "L_TIME CHAR(5)," +
-                        "FREE_DAY INT(1));"));
-
-
-        db.execSQL(String.format("CREATE TABLE MONTH_PLAN (DATE CHAR(10) PRIMARY KEY, WORK_MINUTES INT(4) )"));
-
     }
 
 
@@ -67,17 +64,15 @@ public class DB extends SQLiteOpenHelper {
     }
 
     public List<WorkDay> getWorkDay(String date) {
-        List<WorkDay> workDays = new LinkedList<WorkDay>();
-        String[] kolumny={"LP","DATE","A_TIME","L_TIME","FREE_DAY"};
+        List<WorkDay> workDays = new LinkedList<>();
+        String[] kolumny = {"LP", "DATE", "A_TIME", "L_TIME", "FREE_DAY"};
         SQLiteDatabase db = getReadableDatabase();
-        String args[]={date};
-        Cursor cursor = db.query("REGISTER", kolumny,"date=?", args,null,null,null,null);
-        //Cursor kursor=db.query("telefony",kolumny,"nr=?",args,null,null,null,null);
-
-        while(cursor.moveToNext()){
-            Log.d("DB ","moveToNext0:"+cursor.getInt(0));
+        String args[] = {date};
+        Cursor cursor = db.query("REGISTER", kolumny, "date=?", args, null, null, null, null);
+        while (cursor.moveToNext()) {
+            Log.d("DB ", "moveToNext0:" + cursor.getInt(0));
             WorkDay workDay = new WorkDay();
-            workDay.setLP (cursor.getInt(0));
+            workDay.setLP(cursor.getInt(0));
             workDay.setDate(cursor.getString(1));
             workDay.setArriveTime(cursor.getString(2));
             workDay.setLeavingTime(cursor.getString(3));
@@ -87,25 +82,23 @@ public class DB extends SQLiteOpenHelper {
         return workDays;
     }
 
-    /*
-        public List<WorkDay> getAll() {
-            List<WorkDay> workDays = new LinkedList<>();
-            String[] cols = {"LP", "DATE", "A_TIME", "L_TIME", "FREE_DAY"};
-            SQLiteDatabase db = getReadableDatabase();
-            Cursor kursor = db.query("REGISTER", cols, null, null, null, null, null);
-
-            while (kursor.moveToNext()) {
-                WorkDay workDay = new WorkDay();
-                workDay.setLP(kursor.getInt(0));
-                workDay.setDate(kursor.getString(1));
-                workDay.setArriveTime(kursor.getString(2));
-                workDay.setLeavingTime(kursor.getString(3));
-                workDay.setFreeDay(kursor.getInt(4));
-                workDays.add(workDay);
-            }
-            return workDays;
+    public List<WorkDay> getAll() {
+        List<WorkDay> workDays = new LinkedList<>();
+        String[] cols = {"LP", "DATE", "A_TIME", "L_TIME", "FREE_DAY"};
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor kursor = db.query("REGISTER", cols, null, null, null, null, null);
+        while (kursor.moveToNext()) {
+            WorkDay workDay = new WorkDay();
+            workDay.setLP(kursor.getInt(0));
+            workDay.setDate(kursor.getString(1));
+            workDay.setArriveTime(kursor.getString(2));
+            workDay.setLeavingTime(kursor.getString(3));
+            workDay.setFreeDay(kursor.getInt(4));
+            workDays.add(workDay);
         }
-    */
+        return workDays;
+    }
+
     public void delSetting(String weekDay) {
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL(String.format("DELETE FROM SETTINGS WHERE WEEKDAY='%s'", weekDay));
@@ -143,7 +136,6 @@ public class DB extends SQLiteOpenHelper {
     }
 
     public int getMonthStatus(String date) {
-
         String[] split_date = date.split("-");
         int iYear = Integer.parseInt(split_date[0]);
 
@@ -165,7 +157,6 @@ public class DB extends SQLiteOpenHelper {
             } else {
                 day = Integer.toString(i);
             }
-
 
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm:ss");
             calendar = new GregorianCalendar(2013, 01, 28, 13, 24, 56);
@@ -232,77 +223,23 @@ public class DB extends SQLiteOpenHelper {
         status = status - work_minutes;
         Log.d("DB ", "status:" + status);
         return status;
-        //args[0]=(new SimpleDateFormat("EE").format(d1)).toString().toLowerCase();
-        //String[] col={"WORK_MINUTES"};
-        //cursor = db.query("SETTINGS", col,"weekday=?", args,null,null,null,null);
-
-        //while(cursor.moveToNext()) {
-        //    work_minutes=(cursor.getInt(0));
-        //}
-//        work_minutes = getDaySetting((new SimpleDateFormat("EE").format(d1)).toString().toLowerCase());
-
-
-//            status+=getDateDiff(a_time, l_time);
-//            Log.d("DB ","--------------------");
-//            Log.d("DB ", "a_time:"+a_time);
-//            Log.d("DB ", "l_time:" + l_time);
-//            Log.d("DB ","work_minutes:"+work_minutes);
-//            Log.d("DB ","weekday:"+(new SimpleDateFormat("EE").format(d1)).toString().toLowerCase());
-//            Log.d("DB ","--------------------");
-        //return Integer.parseInt(String.valueOf(0));
     }
-//        status=status-work_minutes;
-//        Log.d("DB ","status:"+date+":"+status);
-//
-//
-//        return status;
-
 
 
     private int getMaxMonthDay(String date) {
 
         String[] split_date = date.split("-");
         int iYear = Integer.parseInt(split_date[0]);
-        int iMonth = Integer.parseInt(split_date[1])-1; //months begin with 0
+        int iMonth = Integer.parseInt(split_date[1]) - 1; //months begin with 0
         int iDay = 1;
-        GregorianCalendar calendar = new GregorianCalendar(iYear,iMonth,iDay);
+        GregorianCalendar calendar = new GregorianCalendar(iYear, iMonth, iDay);
 
         return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 
 
     }
-    //status = status - work_minutes;
-    //Log.d("DB ", "status:" + status);
-    //args[0]=(new SimpleDateFormat("EE").format(d1)).toString().toLowerCase();
-    //String[] col={"WORK_MINUTES"};
-    //cursor = db.query("SETTINGS", col,"weekday=?", args,null,null,null,null);
-
-    //while(cursor.moveToNext()) {
-    //    work_minutes=(cursor.getInt(0));
-    //}
-//        work_minutes = getDaySetting((new SimpleDateFormat("EE").format(d1)).toString().toLowerCase());
-
-
-//            status+=getDateDiff(a_time, l_time);
-//            Log.d("DB ","--------------------");
-//            Log.d("DB ", "a_time:"+a_time);
-//            Log.d("DB ", "l_time:" + l_time);
-//            Log.d("DB ","work_minutes:"+work_minutes);
-//            Log.d("DB ","weekday:"+(new SimpleDateFormat("EE").format(d1)).toString().toLowerCase());
-//            Log.d("DB ","--------------------");
-//     return Integer.parseInt(String.valueOf(0));
-
-//    }
-//        status=status-work_minutes;
-//        Log.d("DB ","status:"+date+":"+status);
-//
-//
-//        return status;
-
-
 
     private int getDateDiff(String date1, String date2) {
-
 
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
@@ -323,7 +260,7 @@ public class DB extends SQLiteOpenHelper {
         return (int) diffMinutes;
     }
 
-    public void addMonthPlan(String date, int [] hours) {
+    public void addMonthPlan(String date, int[] hours) {
 
         SQLiteDatabase db = getWritableDatabase();
 
@@ -343,11 +280,5 @@ public class DB extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //db.execSQL(String.format("CREATE TABLE SETTINGS (WEEKDAY CHAR(3) PRIMARY KEY, WORK_HOURS INT(1) DEFAULT 8 )"));
-        //db.execSQL(String.format("UPDATE SETTINGS SET WEEKDAY='mon', WORK_HOURS='8' where WEEKDAY=0"));
-        //db.execSQL(String.format("INSERT INTO SETTINGS (WEEKDAY,WORK_HOURS) VALUES ('tue','8')"));
-        //db.execSQL(String.format("DELETE SETTINGS WHERE WEEKDAY IN ('33','ala','al','a','ak')"));
     }
-
-
 }
--- app/src/main/java/grupa3/com/niewolnik/MainActivity.java
+++ app/src/main/java/grupa3/com/niewolnik/MainActivity.java
@@ -9,13 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import java.text.DateFormat;
-import java.text.FieldPosition;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -52,126 +48,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-
-        //---------db tests------------------------------------------------------------
         db_manager = new DB(this);
 
-
-        //db_manager.addWorkday(new WorkDay("2016-05-16", "09:30", "15:10", 0));
-        //db_manager.addWorkday(new WorkDay("2016-05-30", "08:30", "12:00", 0));
-        //db_manager.addWorkday(new WorkDay("2016-05-30", "14:30", "17:30", 0));
-
-//        db_manager.addWorkday(new WorkDay("2016-05-26", "07:30", "17:20", 0));
-//        db_manager.addWorkday(new WorkDay("2016-05-30", "08:30", "12:00", 0));
-//        db_manager.addWorkday(new WorkDay("2016-05-30", "14:30", "17:30", 0));
-
-
-        //db_manager.delWorkday("2016-05-26");
-
-        Log.d("DB", " --------pojedynczy----------");
-        for (WorkDay wd : db_manager.getWorkDay("2016-05-26")) {
-            Log.d("DB ", wd.toString());
-        }
-
-        Log.d("DB", " --------wyswietlanie----------");
-//        for (WorkDay wd : db_manager.getAll()) {
-//            Log.d("DB ", wd.toString());
-//        }
-        Log.d("DB", " --------settings----------");
-
-        db_manager.updateSetting("mon", 380);
-        //db_manager.addSetting("mon", 480);
-        //db_manager.addSetting("tue", 480);
-        //db_manager.addSetting("wed", 480);
-        //db_manager.addSetting("thu", 480);
-        //db_manager.addSetting("fri", 480);
-        //db_manager.addSetting("sat", 480);
-        //db_manager.addSetting("sun", 480);
-
-        //db_manager.delSetting("fri", 480);
-        //db_manager.delSetting("sat", 480);
-        //db_manager.delSetting("sun",480);
-
-
-//        db_manager.updateSetting("mon", 480);
-//        db_manager.addSetting("mon", 480);
-//        db_manager.addSetting("tue", 480);
-//        db_manager.addSetting("wed", 480);
-//        db_manager.addSetting("thu", 480);
-//        db_manager.addSetting("fri", 480);
-//        db_manager.addSetting("sat", 480);
-//        db_manager.addSetting("sun", 480);
-
-//        db_manager.delSetting("fri", 480);
-//        db_manager.delSetting("sat", 480);
-//        db_manager.delSetting("sun",480);
-        HashMap<String, Integer> allSettings = db_manager.getAllSettings();
-
-        for (String s : new ArrayList<String>(allSettings.keySet())) {
-            Log.d("DB", s + " " + Integer.toString(allSettings.get(s)));
-
-        }
-        Log.d("DB", " --------day setting----------");
-        Log.d("DB", Integer.toString(db_manager.getDaySetting("mon")));
-
-
-        Log.d("DB", " --------day status----------");
-        db_manager.getDayStatus("2016-05-26");
-        Log.d("DB", " --------day status----------");
-        db_manager.getDayStatus("2016-05-30");
-        Log.d("DB", " --------month status----------");
-        Log.d("DB", "month:" + Integer.toString(db_manager.getMonthStatus("2016-05")));
-
-//        Log.d("DB", " --------day status----------");
-//        db_manager.getDayStatus("2016-05-26");
-//        Log.d("DB", " --------day status----------");
-//        db_manager.getDayStatus("2016-05-30");
-
-
-        //--------------------------------------------------------------------
     }
 
 
     public void startWorkingTime(View view) {
-        StringBuffer dataWStringu = new StringBuffer();
         Date todayDate = new Date(System.currentTimeMillis());
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        List<WorkDay> workDayList = db_manager.getWorkDay(sdf.format(todayDate));
 
-
-        if (((LinkedList) db_manager.getWorkDay(sdf.format(todayDate))).size() != 0) {
-
-            if (((LinkedList) db_manager.getWorkDay(sdf.format(todayDate))).size() != 0) {
+        if (workDayList.size() != 0) {
+            if ((db_manager.getWorkDay(sdf.format(todayDate))).size() != 0) {
 
                 Log.d("DB", " --------day status----------");
-                List<WorkDay> workDayList = new ArrayList<>();
-                workDayList.addAll(db_manager.getWorkDay(sdf.format(todayDate).toString()));
+                workDayList.addAll(db_manager.getWorkDay(sdf.format(todayDate)));
                 for (int i = 0; i < workDayList.size(); i++) {
                     Log.d("DB", workDayList.get(i).toString());
                 }
             } else {
                 db_manager.addWorkday(new WorkDay());
                 Log.d("DB", " --------day added ----------");
-                Log.d("DB", db_manager.getWorkDay(sdf.format(todayDate).toString()).toString());
+                Log.d("DB", db_manager.getWorkDay(sdf.format(todayDate)).toString());
             }
-            //if(db_manager.getWorkDay())
-            //db_manager.addWorkday(new WorkDay());
         }
-
-        if (((LinkedList) db_manager.getWorkDay(sdf.format(todayDate))).size() != 0) {
-            Log.d("DB", " --------day status----------");
-            List<WorkDay> workDayList = new ArrayList<>();
-            workDayList.addAll(db_manager.getWorkDay(sdf.format(todayDate).toString()));
-            for (int i = 0; i < workDayList.size(); i++) {
-                Log.d("DB", workDayList.get(i).toString());
-            }
-        } else {
-            db_manager.addWorkday(new WorkDay());
-            Log.d("DB", " --------day added ----------");
-            Log.d("DB", db_manager.getWorkDay(sdf.format(todayDate).toString()).toString());
-        }
-        //if(db_manager.getWorkDay())
-        //db_manager.addWorkday(new WorkDay());
-
     }
 
     public void stopWorkingTime(View view) {
@@ -209,5 +109,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
 }
